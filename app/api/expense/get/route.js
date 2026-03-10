bbcdb192ef153668b27ee6c3158e2d6f15caeb39
Shruti Deshmukh <your_email@example.com>
2026-03-10 16:58:36 +0530

    feat: create API route to fetch user expenses

---
+++ app/api/expense/get/route.js
@@ -0,0 +1,18 @@
+import { connectDB } from "@/lib/mongodb";
+import Expense from "@/models/Expense";
+
+export async function GET() {
+  try {
+    await connectDB();
+    const expenses = await Expense.find().sort({ date: -1 });
+    return Response.json({
+      success: true,
+      expenses,
+    });
+  } catch (error) {
+    return Response.json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
